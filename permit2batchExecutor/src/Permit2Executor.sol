f309f6a829b87e757597e6ce1e8eeb7f8e1d73b8
blablalf <contact@alfred-gaillard.fr>
2024-01-26 23:43:42 +0100

    Importing required libs and extending required contracts

--- permit2batchExecutor/src/Permit2Executor.sol
+++ permit2batchExecutor/src/Permit2Executor.sol
@@ -4,10 +4,28 @@ pragma solidity ^0.8.21;
 import {
     IPermit2, ISignatureTransfer
 } from "permit2/src/interfaces/IPermit2.sol";
+import {
+    Ownable,
+    Ownable2Step
+} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {
+    ERC20
+} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { SafeERC20 } from
+    "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { Pausable } from
+    "@openzeppelin/contracts/utils/Pausable.sol";
 
-contract Permit2Executor {
-    function execute(address target, bytes memory data) external payable {
-        (bool success, bytes memory returndata) = target.call{value: msg.value}(data);
-        require(success, string(returndata));
-    }
+struct Permit2Params {
+    uint256 amount;
+    uint256 nonce;
+    uint256 deadline;
+    address token;
+    bytes signature;
+}
+
+contract Permit2Executor is Pausable, Ownable2Step {
+
+    constructor() Ownable(_msgSender()) {}
+
 }
