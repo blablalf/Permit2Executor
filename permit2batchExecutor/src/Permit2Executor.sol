c3593606e90c30bd066edb9234d263eff24a9990
blablalf <contact@alfred-gaillard.fr>
2024-01-26 23:38:19 +0100

    Sertting file structures, oz and permit2 libs, permit2 import, remappings

--- permit2batchExecutor/src/Permit2Executor.sol
+++ permit2batchExecutor/src/Permit2Executor.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.21;
 
+import {
+    IPermit2, ISignatureTransfer
+} from "permit2/src/interfaces/IPermit2.sol";
+
 contract Permit2Executor {
     function execute(address target, bytes memory data) external payable {
         (bool success, bytes memory returndata) = target.call{value: msg.value}(data);
